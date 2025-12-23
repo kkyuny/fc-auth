3b9404560e6638eb5e6b9b441e07a947342e3012
kyun <kyun9911@gmail.com>
2025-12-23 10:45:13 +0900

    [5-4] 시스템 별 권한 세팅

---
+++ src/main/java/com/example/fc_auth/model/AppRole.java
@@ -0,0 +1,20 @@
+package com.example.fc_auth.model;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.persistence.*;
+import lombok.Getter;
+
+import java.util.List;
+
+@Entity
+@Getter
+public class AppRole {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Schema(example = "123", description = "auto increment pk")
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "api_id", referencedColumnName = "id")
+    private Api api;
+}
