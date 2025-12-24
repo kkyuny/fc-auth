889de59a32e63d3f9bdb7c9d2f8fefdf7c93abed
kyun <kyun9911@gmail.com>
2025-12-24 17:11:05 +0900

    [5-5] import 수정

--- src/main/java/com/example/fc_auth/controller/AppTokenController.java
+++ src/main/java/com/example/fc_auth/controller/AppTokenController.java
@@ -1,6 +1,7 @@
 package com.example.fc_auth.controller;
 
 import com.example.fc_auth.dto.AppTokenRespDto;
+import com.example.fc_auth.dto.ValidateTokenDto;
 import com.example.fc_auth.service.TokenService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
